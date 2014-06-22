474a18c315a75e6d2f62f54a6c664483748042f7
Kris Craig <kriscraig@php.net>
2014-06-22 00:24:10 -0700

    User-related classes.

--- example/classes/users/friends/friends.class.php
+++ example/classes/users/friends/friends.class.php
@@ -5,9 +5,39 @@ class friends extends username
 	public static $info = "Read or write data pertaining to the specified username's friends list.";
 	
 	/* Get all the user's friends.  --Kris */
-	public static function GET()
+	public static function GET( $username, $limit = 25, $page = 1, $offset = NULL )
 	{
+		try
+		{
+			require_once( "SQL/config_sql.class.php" );
+			$sql = (new Config_SQL)->load_db();
+		}
+		catch ( Exception $e )
+		{
+			return array( "status" => 503, "error" => "Unable to connect to database : " . $e->getMessage() );
+		}
 		
+		if ( $offset === NULL )
+		{
+			$offset = $limit * ($page - 1);
+		}
+		
+		try
+		{
+			$data = $sql->query( "SELECT username1, username2 FROM friends WHERE username1 = ? OR username2 = ? LIMIT ? OFFSET ?", 
+					array( $username, $username, $limit, $offset ) );
+		}
+		catch ( Exception $e )
+		{
+			return array( "status" => 500, "error" => "SQL error : " . $e->getMessage() );
+		}
+		
+		foreach ( $data as &$entry )
+		{
+			$entry = Dispatch::URI( "friend_username", $entry[( strcasecmp( $username, $entry["username1"] ) ? "username1" ? "username2" )] );  // Translate into API call.  --Kris
+		}
+		
+		return array( "status" => 200, "data" => $data );
 	}
 	
 	public static function POST()
@@ -21,8 +51,27 @@ class friends extends username
 	}
 	
 	/* Delete ALL of this user's friends.  That's not very friendly, ya know.  --Kris */
-	public static function DELETE()
+	public static function DELETE( $username )
 	{
+		try
+		{
+			require_once( "SQL/config_sql.class.php" );
+			$sql = (new Config_SQL)->load_db();
+		}
+		catch ( Exception $e )
+		{
+			return array( "status" => 503, "error" => "Unable to connect to database : " . $e->getMessage() );
+		}
 		
+		try
+		{
+			$affrows = $sql->query( "DELETE FROM friends WHERE username1 = ? OR username2 = ?", array( $username, $username ), SQL_RETURN_AFFECTEDROWS );
+		}
+		catch ( Exception $e )
+		{
+			return array( "status" => 500, "error" => "SQL error : " . $e->getMessage() );
+		}
+		
+		return ( $affrows > 0 ? 204 : 404 );
 	}
 }
--- example/classes/users/men.class.php
+++ example/classes/users/men.class.php
@@ -5,9 +5,38 @@ class men extends users
 	public static $info = "Read or write data pertaining to male users.";
 	
 	/* Get all male users.  --Kris */
-	public static function GET()
+	public static function GET( $limit = 25, $page = 1, $offset = NULL )
 	{
+		try
+		{
+			require_once( "SQL/config_sql.class.php" );
+			$sql = (new Config_SQL)->load_db();
+		}
+		catch ( Exception $e )
+		{
+			return array( "status" => 503, "error" => "Unable to connect to database : " . $e->getMessage() );
+		}
 		
+		if ( $offset === NULL )
+		{
+			$offset = $limit * ($page - 1);
+		}
+		
+		try
+		{
+			$data = $sql->query( "SELECT username FROM users WHERE gender = ? LIMIT ? OFFSET ?", array( 'male', $limit, $offset ) );
+		}
+		catch ( Exception $e )
+		{
+			return array( "status" => 500, "error" => "SQL error : " . $e->getMessage() );
+		}
+		
+		foreach ( $data as &$entry )
+		{
+			$entry = Dispatch::URI( "username", $entry );  // Translate username into API call for that username.  --Kris
+		}
+		
+		return array( "status" => 200, "data" => $data );
 	}
 	
 	public static function POST()
@@ -16,14 +45,55 @@ class men extends users
 	}
 	
 	/* Update all male users.  --Kris */
-	public static function PUT()
+	public static function PUT( $status )
 	{
+		if ( !is_numeric( $status ) || intval( $status ) != $status || abs( $status ) > 999 )
+		{
+			return 400;
+		}
 		
+		try
+		{
+			require_once( "SQL/config_sql.class.php" );
+			$sql = (new Config_SQL)->load_db();
+		}
+		catch ( Exception $e )
+		{
+			return array( "status" => 503, "error" => "Unable to connect to database : " . $e->getMessage() );
+		}
+		
+		try
+		{
+			return array( "status" => 200, "data" => array( "AFFECTED_ROWS" => $sql->query( "UPDATE users SET status = ? WHERE gender = ?", 
+				array( $status, 'male' ), SQL_RETURN_AFFECTEDROWS ) ) );
+		}
+		catch ( Exception $e )
+		{
+			return array( "status" => 500, "error" => "SQL error : " . $e->getMessage() );
+		}
 	}
 	
 	/* Delete all male users.  --Kris */
 	public static function DELETE()
 	{
+		try
+		{
+			require_once( "SQL/config_sql.class.php" );
+			$sql = (new Config_SQL)->load_db();
+		}
+		catch ( Exception $e )
+		{
+			return array( "status" => 503, "error" => "Unable to connect to database : " . $e->getMessage() );
+		}
 		
+		try
+		{
+			return array( "status" => 200, "data" => array( "AFFECTED_ROWS" => $sql->query( "DELETE FROM users WHERE gender = ?", 
+				array( $status, 'male' ), SQL_RETURN_AFFECTEDROWS ) ) );
+		}
+		catch ( Exception $e )
+		{
+			return array( "status" => 500, "error" => "SQL error : " . $e->getMessage() );
+		}
 	}
 }
