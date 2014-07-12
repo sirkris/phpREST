4f5f2c49226adb02f3d4764b359037bbc9a5388b
Kris Craig <kriscraig@php.net>
2014-07-12 00:28:47 -0700

    Added empty dispatch config class in top-level.

---
+++ config.dispatch.php
@@ -0,0 +1,20 @@
+<?php
+
+class Config_Dispatch
+{
+	public static $classes = array( 
+					
+				);
+	
+	public static $class_paths = array( 
+					
+				);
+	
+	public static $class_manual = array( 
+					
+				);
+	
+	public static $class_args = array( 
+					
+				);
+}
